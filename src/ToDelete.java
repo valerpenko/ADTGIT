2b7292559d7a4157f2641a2820aff688844fcc2e
valerpenko <vpenko@onu.edu.ua>
2020-05-05 19:16:59 +0300

    Hello world

---
+++ src/ToDelete.java
@@ -0,0 +1,9 @@
+//import java.lang.*;
+//import java.util.*;
+public class ToDelete
+{
+    public static void main(String[] args) {
+        System.out.println("Я изучаю");
+        System.out.println ("Java");
+    }
+}
