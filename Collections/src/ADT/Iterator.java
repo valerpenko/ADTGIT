f40ffd7b434292eafebf1ebd10e822efbc9a88d1
Ilya555 <ilya92702@gmail.com>
2020-05-08 18:16:47 +0300

    Iterator interface modified

--- Collections/src/ADT/Iterator.java
+++ Collections/src/ADT/Iterator.java
@@ -1,11 +1,8 @@
 package ADT;
 import java.util.NoSuchElementException;
 
-<<<<<<< HEAD
+
 public interface Iterator <E> extends java.util.Iterator<E>
-=======
-public interface Iterator <E>  extends java.util.Iterator
->>>>>>> e98679d605d3dc318b0e60f740f69be157d55a41
 {
     boolean hasNext();
     E next() throws NoSuchElementException;
