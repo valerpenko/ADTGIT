e5af8d5ad97e6b23f7682ae1cd2e9cb470e65d9f
valerpenko <vpenko@onu.edu.ua>
2020-07-17 19:51:11 +0300

    SpellChecker prototype

--- Collections/src/listbased/SkipListSortedMap.java
+++ Collections/src/listbased/SkipListSortedMap.java
@@ -1,62 +1,62 @@
-package listbased;
-
-import ADT.AbstractSortedMap;
-import ADT.Position;
-import ADT.SkipList;
-import listbased.DoublyLinkedList;
-
-import java.util.ArrayList;
-import java.util.Random;
-
-public class SkipListSortedMap<K,V> extends AbstractSortedMap<K,V> implements SkipList<K>
-{
-    SkipList skipList=new LinkedSkipList();
-
-
-    class LinkedSkipList<E> implements SkipList<E>
-    {
-        ArrayList <DoublyLinkedList<E>> horizontalLists;  // approx ln n
-        DoublyLinkedList<E>[] verticalLists;    // n+2
-        private Random rnd= new Random();
-
-        public  LinkedSkipList(ArrayList<E> source)
-        {
-            //see Goodrich p.436
-            int level =0;
-            DoublyLinkedList<E> hlBase=new DoublyLinkedList<E>();
-            DoublyLinkedList<E> hlAbove;
-            //fill hl with source
-            hlBase.addFirst(source.get(0));
-            for(int i = 1; i < source.size(); i++) {hlBase.addLast(source.get(i));}
-
-            while(hlBase.size()>1)
-            {
-                hlAbove = MakeLevelAbove(hlBase);
-                horizontalLists.add(hlAbove);
-                hlBase=hlAbove;
-                level++;
-            }
-        }
-        private DoublyLinkedList<E> MakeLevelAbove(DoublyLinkedList<E> hlBase)
-        //fills level randomly with subset from hlBase
-        //makes vertical links for towers
-        {
-            DoublyLinkedList<E> levelAbove = new DoublyLinkedList<E>();
-            E item=hlBase.first();
-            while (item!=null)
-            {
-                if (rnd.nextBoolean())
-                {
-
-                }
-                item=hlBase.
-            }
-        }
-
-        Position<E> next(Position<E> p); //Returns the position following p on the same level.
-        Position<E> prev(Position<E>p); //Returns the position preceding p on the same level.
-        Position<E> above(Position<E>p); //Returns the position above p in the same tower.
-        Position<E> below(Position<E>p); //Returns the position below p in the same tower.
-
-    }
-}
+//package listbased;
+//
+//import ADT.AbstractSortedMap;
+//import ADT.Position;
+//import ADT.SkipList;
+//import listbased.DoublyLinkedList;
+//
+//import java.util.ArrayList;
+//import java.util.Random;
+//
+//public class SkipListSortedMap<K,V> extends AbstractSortedMap<K,V> implements SkipList<K>
+//{
+//    SkipList skipList=new LinkedSkipList();
+//
+//
+//    class LinkedSkipList<E> implements SkipList<E>
+//    {
+//        ArrayList <DoublyLinkedList<E>> horizontalLists;  // approx ln n
+//        DoublyLinkedList<E>[] verticalLists;    // n+2
+//        private Random rnd= new Random();
+//
+//        public  LinkedSkipList(ArrayList<E> source)
+//        {
+//            //see Goodrich p.436
+//            int level =0;
+//            DoublyLinkedList<E> hlBase=new DoublyLinkedList<E>();
+//            DoublyLinkedList<E> hlAbove;
+//            //fill hl with source
+//            hlBase.addFirst(source.get(0));
+//            for(int i = 1; i < source.size(); i++) {hlBase.addLast(source.get(i));}
+//
+//            while(hlBase.size()>1)
+//            {
+//                hlAbove = MakeLevelAbove(hlBase);
+//                horizontalLists.add(hlAbove);
+//                hlBase=hlAbove;
+//                level++;
+//            }
+//        }
+//        private DoublyLinkedList<E> MakeLevelAbove(DoublyLinkedList<E> hlBase)
+//        //fills level randomly with subset from hlBase
+//        //makes vertical links for towers
+//        {
+//            DoublyLinkedList<E> levelAbove = new DoublyLinkedList<E>();
+//            E item=hlBase.first();
+//            while (item!=null)
+//            {
+//                if (rnd.nextBoolean())
+//                {
+//
+//                }
+//                item=hlBase.
+//            }
+//        }
+//
+//        Position<E> next(Position<E> p); //Returns the position following p on the same level.
+//        Position<E> prev(Position<E>p); //Returns the position preceding p on the same level.
+//        Position<E> above(Position<E>p); //Returns the position above p in the same tower.
+//        Position<E> below(Position<E>p); //Returns the position below p in the same tower.
+//
+//    }
+//}
--- SetClient/src/SpellCheck.java
+++ SetClient/src/SpellCheck.java
@@ -7,7 +7,9 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Scanner;
 
 class SpellChecker
@@ -17,7 +19,8 @@ class SpellChecker
     {
         Scanner scan = new Scanner(System.in);
         String word = scan.nextLine();
-        SpellChecker.Check(word);
+        Set<String> prompts = SpellChecker.Check(word);
+
         //show proposed words
     }
 
@@ -25,23 +28,32 @@ class SpellChecker
     {
         dictionary = new ArraySet<String>(100000);
 
-        Path p = Paths.get("D:/desktop/dictionary.txt");
+        Path p = Paths.get("D:\\Projects\\Java\\ADTGIT\\SetClient\\dictionary.txt");
         BufferedReader reader = null;
         try {
             reader = Files.newBufferedReader(p);
         } catch (IOException e) {
             e.printStackTrace();
         }
-        String line = null;
+        String line=null;
         try {
             line = reader.readLine();
         } catch (IOException e) {
             e.printStackTrace();
         }
-        try {
-            dictionary.add(line);
-        } catch (SetFullException e) {
-            e.printStackTrace();
+        while(line!=null)
+        {
+            try {
+                dictionary.add(line);
+            } catch (SetFullException e) {
+                e.printStackTrace();
+            }
+
+            try {
+                line = reader.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 //    public  SpellChecker() throws IOException, SetFullException
@@ -55,42 +67,64 @@ class SpellChecker
 //    }
     public static Set<String> Check(String word) throws SetFullException
     {
-        Set<String> result= new ArraySet<>();
+        Set<String> result= new ArraySet<>(100);
 
         if (dictionary.contains(word))
         {
-            result.add(word);
-            return result;
+            return result; //empty!
         }
-        else{
-            //MisspellingIterator<String> mIterator = new MisspellingIterator<>(word);
-            Iterator<String> mIterator = dictionary.iterator();
+        else
+        {
+            MisspellingIterator<String> mIterator = new MisspellingIterator<>(word);
             //String mp = null;
             while(mIterator.hasNext())//mp=mIterator.hasNext()!=null
             {
-                //if (dictionary.contains(mIterator.next()));
-                if (mIterator.next().compareTo(word)>1)
-                    result.add(mIterator.next());
+                String w=mIterator.Next();
+                if (dictionary.contains(w))
+                    result.add(w);
             }
             return result;
         }
     }
     private static class MisspellingIterator<E>
     {
-        private ArraySet<String> mispellings = new ArraySet<>();
-        Iterator<String> iter = mispellings.iterator();
+        private List<String> mispellings = new ArrayList<>();
+        private int currIndex=-1;
+        //Iterator<String> iter = mispellings.iterator();
 
         public MisspellingIterator(String word) throws SetFullException
         {
             mispellings.add(word);//fill misspelling
+            AddExtraChar(word);
+            //DeleteOneChar(word);
+            //   ???
+
+        }
+        private void AddExtraChar(String word)
+        {
+            mispellings.add("a"+word);
+
+            mispellings.add(word.substring(0,2)+"a"+word.substring(3));
+
+            mispellings.add(word+"a");
+
         }
         public String Next()
         {
-            return iter.next();
+            if(hasNext())
+            {
+                currIndex++;
+                return mispellings.get(currIndex);
+            }
+            else return null;
+
         }
         public boolean hasNext()
         {
-            return iter.hasNext();
+            if (currIndex<mispellings.size()-1)
+                return true;
+            else
+                return false;
         }
     }
 }
