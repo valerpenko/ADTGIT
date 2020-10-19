40419507c09122950343820cefea8f6a249c73d0
John <ilya92702@gmail.com>
2020-10-19 15:20:49 +0300

    Attempting to print the process of mergeSort

--- sorting/src/BottomUpMergeSort.java
+++ sorting/src/BottomUpMergeSort.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Comparator;
 
 public class BottomUpMergeSort
@@ -11,12 +12,21 @@ public class BottomUpMergeSort
         int y=start+inc; // index into run 2
         int z=start; // index into output
         while (x < end1 && y < end2)
-            if (comp.compare(in[x], in[y]) < 0)
-            out[z++] = in[x++]; // take next from run 1
+            if (comp.compare(in[x], in[y]) < 0) {
+                out[z++] = in[x++]; // take next from run 1
+//                System.out.println("First half");
+//                System.out.println(Arrays.toString(out));
+            }
         else
         out[z++] = in[y++]; // take next from run 2
-        if (x < end1) System.arraycopy(in, x, out, z, end1 - x); // copy rest of run 1
+//        System.out.println("Second half");
+//        System.out.println(Arrays.toString(out));
+        if (x < end1) {System.arraycopy(in, x, out, z, end1 - x);
+            System.out.println("First half");
+            System.out.println(Arrays.toString(out));} // copy rest of run 1
         else if (y < end2) System.arraycopy(in, y, out, z, end2 - y); // copy rest of run 2
+        System.out.println("Second half");
+        System.out.println(Arrays.toString(out));
     }
     // Merge-sort contents of data array.
     public static <K> void mergeSortBottomUp(K[ ] orig, Comparator<K> comp)
@@ -28,7 +38,10 @@ public class BottomUpMergeSort
         for (int i=1; i < n; i *= 2)
         {   // each iteration sorts all runs of length i
             for (int j=0; j < n; j += 2*i) // each pass merges two runs of length i
-            merge(src, dest, comp, j, i);
+            {
+                merge(src, dest, comp, j, i);
+                //MergePrint();
+            }
             temp = src; src = dest; dest = temp; // reverse roles of the arrays
         }
         if (orig != src)
---
+++ sorting/src/BottomUpSortClient.java
@@ -0,0 +1,16 @@
+import ADT.DefaultComparator;
+
+import java.util.Comparator;
+
+public class BottomUpSortClient
+{
+    public static void main(String[] args) throws Exception
+    {
+        Integer[] numbers = {1, 23, 4, 11, 50, 15, 17, 9, 2, 10};
+
+        Comparator<Integer> comp = new DefaultComparator<>();
+        BottomUpMergeSort.mergeSortBottomUp(numbers, comp);
+//        for(Integer i : numbers)
+//            System.out.println(i);
+    }
+}
