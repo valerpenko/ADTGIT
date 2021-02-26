5ce0d93b013bee4d9ef091f578e503e32e7d805a
John <ilya92702@gmail.com>
2021-02-26 16:06:57 +0300

    Huffman class added

---
+++ TextProcessing/src/Huffman.java
@@ -0,0 +1,35 @@
+
+import ADT.Entry;
+import arraybased.UnsortedTableMap;
+import listbased.LinkedBinaryTree;
+import listbased.UnsortedPriorityQueue;
+
+public class Huffman
+{
+    public static void main(String[] args)
+    {
+        UnsortedTableMap<Character, Integer> map = new UnsortedTableMap<>();
+        String s = "Unsorted Priority Queue";
+        for (int i = 0; i < s.length(); i++)
+        {
+            //take each character
+            char ch = s.charAt(i);
+            //check if already in map
+            Integer val = map.get(ch);
+            if (val != null)
+            {
+                map.put(ch, val + 1);
+            }
+            else
+            {
+                map.put(ch, 1);
+            }
+        }
+        for (Entry<Character, Integer> obj : map.entrySet())
+            System.out.println(obj.getKey() + "-" + obj.getValue());
+
+        UnsortedPriorityQueue<Character, Integer> queue = new UnsortedPriorityQueue<>();
+
+        LinkedBinaryTree<Entry<Integer,Character>> charTree = new LinkedBinaryTree<>();
+    }
+}
