bfbf9fbabc5ebfb696c78b912b96e7dbe459172f
John <ilya92702@gmail.com>
2021-06-11 16:02:25 +0300

    XO class added

---
+++ src/XO.java
@@ -0,0 +1,45 @@
+import java.util.Arrays;
+
+public class XO
+{
+    public static void main(String[] args)
+    {
+        int X = 1;
+        int O = 0;
+        int Empty = -1;
+
+        int [][] Board = {{Empty,Empty,Empty},
+                          {Empty,Empty,Empty},
+                          {Empty,Empty,Empty}};
+
+        for (int[] row : Board)
+            System.out.println(Arrays.toString(row));
+
+        Show(Board);
+    }
+
+
+
+    public static void Show(int [][] Board)
+    {
+        for (int[] row : Board)
+        {
+            for (int cell : row)
+            {
+                if (cell == 1)
+                {
+                    System.out.println("X");
+                }
+                else if (cell == 0)
+                {
+                    System.out.println("O");
+                }
+                else
+                {
+                    System.out.println("_");
+                }
+            }
+        }
+    }
+
+}
