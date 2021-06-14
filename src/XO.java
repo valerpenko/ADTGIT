f121ef5c38f674fdb6d043108eb4c0f8b72353df
valerpenko <vpenko@onu.edu.ua>
2021-06-14 17:54:42 +0300

    XO Model semi finished

--- src/XO.java
+++ src/XO.java
@@ -1,6 +1,50 @@
 import java.util.Arrays;
+enum XOCell {X,O,Empty}
+enum XOStatus {XWin,OWin,Draw,Continue}
+class XOModel
+{
+    XOCell[][] board;
+    boolean XTurn;
+    int moveCount;
+    int height;
+    int width;
+    public XOModel(int h,int w)
+    {
+        height = h; width = w;
+        board=new XOCell[height][width];
+        XTurn=true;
+        moveCount=0;
+    }
+    public XOModel()
+    {
+        this(3,3);
+    }
+    public void Move(int row, int col)
+    {
+        if (board[row][col]==XOCell.Empty) {
+            board[row][col] = (XTurn ? XOCell.X : XOCell.O);
+            XTurn = !XTurn;
+            moveCount++;
+        }
+    }
+    public  XOStatus GameStatus()
+    {
+        if (victory(XTurn)) {
+            if (XTurn)
+                return XOStatus.XWin;
+            else
+                return XOStatus.OWin;
+        }
+        if(moveCount < height*width)
+        {
+            return XOStatus.Draw;
+        }
+        else {return XOStatus.Continue;}
 
-class XOModel {
+    }
+
+    private boolean victory(boolean player)
+    {return true;}
 }
 class XOView
 {
@@ -38,6 +82,6 @@ public class XO {
 
         //for (int[] row : Board)
         System.out.println(Arrays.toString(Board));
-        Show(Board);
+        //Show(Board);
     }
 }
